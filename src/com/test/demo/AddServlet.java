77430a0c0cea0d6b08bc9ad299a6438847fcfe39
Administrator <Administrator@DESKTOP-K9KHEAV>
2022-02-19 19:00:22 +0900

    주석 제거

--- src/com/test/demo/AddServlet.java
+++ src/com/test/demo/AddServlet.java
@@ -9,27 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
-	/*public void service(HttpServletRequest request , HttpServletResponse response) {
-		int i = Integer.parseInt(request.getParameter("num1"));
-		int j = Integer.parseInt(request.getParameter("num2"));
-		
-		int k = i + j;
-		System.out.println("result is " + k);
-		
-		git test
-		branch test
-		hh
-		브랜치 커밋은 오랜만...
-		test2
-		
-		try {
-			PrintWriter out = response.getWriter();
-			out.println("result is " + k);
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-	}*/
-	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int i = Integer.parseInt(request.getParameter("num1"));
