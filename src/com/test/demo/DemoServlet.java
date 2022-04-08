c5f5470ab38abae6b86634a1fd33298977aba61b
Administrator <Administrator@DESKTOP-K9KHEAV>
2022-04-09 01:13:55 +0900

    JSTL Tutorial Core Tags(Part 2)

--- src/com/test/demo/DemoServlet.java
+++ src/com/test/demo/DemoServlet.java
@@ -1,6 +1,8 @@
 package com.test.demo;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -16,8 +18,26 @@ public class DemoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// super.doGet(req, resp);
 		
-		String name = "Minu";
-		req.setAttribute("label", name);
+		// String name = "Minu";
+		
+		List<EmployeeVO> empList = new ArrayList<>();
+		EmployeeVO emp = new EmployeeVO();
+		// 1001 , 박민우, 경기도 안산시, 31
+		emp.setEmpNo(1001);
+		emp.setEmpName("박민우");
+		emp.setEmpAddr("경기도 안산시");;
+		emp.setEmpAge(31);
+		empList.add(emp);
+		emp = new EmployeeVO();
+		emp.setEmpNo(1002);
+		emp.setEmpName("한채희");
+		emp.setEmpAddr("서울시 송파구");;
+		emp.setEmpAge(31);
+		empList.add(emp);
+		// req.setAttribute("employee", emp);
+		req.setAttribute("employeeList", empList);
+		
+		// req.setAttribute("label", name);
 		RequestDispatcher rd = req.getRequestDispatcher("display.jsp");
 		rd.forward(req, resp);
 	}
